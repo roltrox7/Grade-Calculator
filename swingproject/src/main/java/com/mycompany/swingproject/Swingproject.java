a546faea5a12bc53d9d98f9b94bce355b29f9d24
roltrox7 <107786410+roltrox7@users.noreply.github.com>
2023-01-14 09:16:47 +0530

    Uploaded files

---
+++ swingproject/src/main/java/com/mycompany/swingproject/Swingproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.swingproject;
+
+/**
+ *
+ * @author roltrox
+ */
+public class Swingproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
